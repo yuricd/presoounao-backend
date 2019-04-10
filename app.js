82acb2e5598afe8d1c2c8b8600fcdfdcc66ee60f
Yuri Delgado <ydelgado@localhost.localdomain>
2019-04-09 22:09:04 -0300

    Using cors lib

--- app.js
+++ app.js
@@ -14,20 +14,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain);
+app.use(cors());
 
 app.use('/static', express.static('public'));
 
